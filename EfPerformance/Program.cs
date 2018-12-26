456b1564f192af4d44b9801059a9b7299b4035ab
rushi <rushi.2106@gmail.com>
2018-12-26 16:11:04 +0530

    Initial

---
+++ EfPerformance/Program.cs
@@ -0,0 +1,184 @@
+﻿using EFCore.BulkExtensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace EfPerformance
+{
+    internal class Program
+    {
+        private static readonly int iterationCount = 1000;
+
+        private static void Main(string[] args)
+        {
+
+            Case1();
+
+            Case1_1();
+
+            Case2();
+
+            Case3();
+
+            Case4();
+
+            Case5();
+
+            Console.ReadLine();
+        }
+
+        //Add and SaveChanges inside loop
+        private static void Case1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            using (var dbContext = new TestContext())
+            {
+                for (var i = 0; i < iterationCount; i++)
+                {
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    dbContext.Person.Add(person);
+
+                    dbContext.SaveChanges();
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 1 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+        //Add and SaveChanges inside loop with new DbContext for every operation
+        private static void Case1_1()
+        {
+            var sw = Stopwatch.StartNew();
+
+
+            for (var i = 0; i < iterationCount; i++)
+            {
+                using (var dbContext = new TestContext())
+                {
+
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    dbContext.Person.Add(person);
+
+                    dbContext.SaveChanges();
+                }
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 1_1 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+        //Add inside loop, SaveChanges outside loop
+        private static void Case2()
+        {
+            var sw = Stopwatch.StartNew();
+
+            using (var dbContext = new TestContext())
+            {
+                for (var i = 0; i < iterationCount; i++)
+                {
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    //Change tracking started
+                    dbContext.Person.Add(person);
+                }
+
+                dbContext.SaveChanges();
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 2 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+        //Add inside loop, SaveChanges outside loop, AutoDetectionChangesEnabled = false
+        private static void Case3()
+        {
+            var sw = Stopwatch.StartNew();
+
+            using (var dbContext = new TestContext())
+            {
+                dbContext.ChangeTracker.AutoDetectChangesEnabled = false;
+
+                for (var i = 0; i < iterationCount; i++)
+                {
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    //Change tracking started
+                    dbContext.Person.Add(person);
+                }
+
+                dbContext.SaveChanges();
+               
+                dbContext.ChangeTracker.AutoDetectChangesEnabled = true;
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 3 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+        //Temporary list inside loop, AddRange at once outside loop, SaveChanges outside loop
+        private static void Case4()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var tempPersons = new List<Person>();
+
+            using (var dbContext = new TestContext())
+            {
+                for (var i = 0; i < iterationCount; i++)
+                {
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    tempPersons.Add(person);
+                }
+
+                dbContext.Person.AddRange(tempPersons);
+
+                dbContext.SaveChanges();
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 4 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+        //SqlBulkCopy
+        private static void Case5()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var tempPersons = new List<Person>();
+
+            using (var dbContext = new TestContext())
+            {
+                for (var i = 0; i < iterationCount; i++)
+                {
+                    var person = new Person();
+                    person.Name = "Person " + i;
+
+                    tempPersons.Add(person);
+                }
+
+                //Bulk insert at once using SqlBulkCopy
+                dbContext.BulkInsert(tempPersons);
+            }
+
+            sw.Stop();
+
+            Console.WriteLine("Case 5 Took:" + sw.ElapsedMilliseconds + "ms");
+        }
+
+    }
+}
---
+++ EfPerformance/TestContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfPerformance
+{
+    public class TestContext : DbContext
+    {
+        public static readonly LoggerFactory MyLoggerFactory = new LoggerFactory(new[] { new ConsoleLoggerProvider((category, level)
+                                                                                                => level >= LogLevel.Debug, true) });
+
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging(true);
+
+            optionsBuilder
+                .UseLoggerFactory(MyLoggerFactory)
+                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=EfPerformance;Integrated Security=True;MultipleActiveResultSets=true");
+        }
+    }
+}
